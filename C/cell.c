ca433a151087815b1ec6924eb4017a5e2928e76b
Seongjune Yoon <yck93@naver.com>
2022-02-13 17:39:15 +0900

    set

--- C/cell.c
+++ C/cell.c
@@ -1,89 +1,66 @@
 #include <stdio.h>
-#include <time.h> //?‚œ?ˆ˜?ƒ?„±?‹œ ?•„?š”?•œ ?—¤?”
+#include <time.h> //ë‚œìˆ˜ ìƒì„±ì‹œ í•„ìš”
 
 int main(void)
 {
-	/*char name[256];
-	printf("?´ë¦„ì´ ë­ì—?š”? ");
-	scanf_s("%s", name, sizeof(name));
-
-	int age;
-	printf("ëª‡ì‚´?´?—?š”? ");
-	scanf_s("%d", &age);
-
-	float weight;
-	printf("ëª¸ë¬´ê²ŒëŠ” ëª? kg ?´?—?š”? ");
-	scanf_s("%f", &weight);
-
-	double height;
-	printf("?‚¤?Š” ëª? cm ?´?—?š”? ");
-	scanf_s("%lf", &height);
-
-	char what[256];
-	printf("ë¬´ìŠ¨ ë²”ì£„ë¥? ???ì§ˆë???–´?š”? ");
-	scanf_s("%s", what, sizeof(what));
-
-	printf("\n\n==== ë²”ì£„? ? •ë³? ====\n\n");
-	printf(" ?´ë¦?      : %s\n", name);
-	printf(" ?‚˜?´      : %d\n", age);
-	printf(" ëª¸ë¬´ê²?    : %.2fkg\n", weight);
-	printf(" ?‚¤        : %.2lfcm\n", height);
-	printf(" ë²”ì£„ëª?    : %s\n", what);*/
-	
-	//?”¼?¼ë¯¸ë“œë¥? ?Œ“?•„?¼ - ?”„ë¡œì ?Š¸
+	//í”¼ë¼ë¯¸ë“œ ìŒ“ê¸°
 	/*int floor;
-	printf("ëª? ì¸µìœ¼ë¡? ?Œ“ê² ëŠ?ƒ?");
+	printf("ëª‡ì¸µìœ¼ë¡œ ìŒ“ìœ¼ì‹œê² ìŠµë‹ˆê¹Œ?");
 	scanf_s("%d", &floor);
 	int i, j;
-	for (i = 1; i <= floor; i++) {
-		for (j = floor; i < j; j--)
+	fori = 1; i <= floor; i++) {
+		forj = floor; i < j; j--)
 		{
 			printf(" ");
 		}
-		for (j = 0; j < (i*2-1); j++)
+		forj = 0; j <i*2-1); j++)
 		{
 			printf("*");
 		}
 		printf("\n");
 	}*/
 
-	//srand(time(NULL)); // ?‚œ?ˆ˜ ì´ˆê¸°?™”
+
+	//srand(time(NULL)); // ë‚œìˆ˜ ì´ˆê¸°í™”
 	//int num = rand() % 3 +1; // 1 ~ 3
 
-	/*printf("?‚œ?ˆ˜ ì´ˆê¸°?™” ?´? „\n");
-	for (int i = 0; i < 10; i++)
+	/*printf("ë‚œìˆ˜ ì´ˆê¸°í™” ì´ì „\n");
+	forint i = 0; i < 10; i++)
 		printf("%d ", rand() % 10);
 
-	srand(time(NULL));// ?‚œ?ˆ˜ ì´ˆê¸°?™”
-	printf("\n\n?‚œ?ˆ˜ ì´ˆê¸°?™” ?´?›„\n");
+	srand(time(NULL)); //ë‚œìˆ˜ ì´ˆê¸°í™”
+	printf("ë‚œìˆ˜ ì´ˆê¸°í™” ì´í›„\n");
 	for (int i = 0; i < 10; i++)
 		printf("%d ", rand() % 10);*/
 
-	//ê°??œ„ 0 ë°”ìœ„ 1 ë³? 2
+
+	//ê°€ìœ„ ë°”ìœ„ ë³´
 	/*srand(time(NULL));
 	int i = rand() % 3; // 0 ~ 2 ë°˜í™˜
 	switch (i) {
-	case 0:printf("ê°??œ„\n"); break;
+	case 0:printf("ê°€ìœ„\n"); break; //break ì‚¬ìš© ì•ˆí•˜ë©´ ë°‘caseë“¤ë„ ì­‰ ì‹¤í–‰ë¨
 	case 1:printf("ë°”ìœ„\n"); break;
-	case 2:printf("ë³?\n"); break;
-	default:printf("ëª°ë¼?š”\n"); break;
+	case 2:printf("ë³´\n"); break;
+	default:printf("ëª°ë¼ìš”\n"); break;
 	}*/
+
 	
 	/*int age = 15;
-	switch (age) {
+	switch (age)
+	{
 	case 8:
 	case 9:
 	case 10:
 	case 11:
 	case 12:
-	case 13:printf("ì´ˆë“±?•™?ƒ ?…?‹ˆ?‹¤\n"); break;
+	case 13:printf("ì´ˆë“±í•™ìƒ ì…ë‹ˆë‹¤.\n"); break;
 	case 14:
 	case 15:
-	case 16:printf("ì¤‘í•™?ƒ ?…?‹ˆ?‹¤.\n"); break;
+	case 16:printf("ì¤‘í•™ìƒ ì…ë‹ˆë‹¤.\n"); break;
 	case 17:
 	case 18:
-	case 19:printf("ê³ ë“±?•™?ƒ ?…?‹ˆ?‹¤\n"); break;
-	default : printf("?•™?ƒ?´ ?•„?‹ˆ?‹­?‹ˆ?‹¤\n"); break;
+	case 19:printf("ê³ ë“±í•™ìƒ ì…ë‹ˆë‹¤.\n"); break;
+	default : printf("í•™ìƒì´ ì•„ë‹ˆì‹œë„¤ìš”\n"); break;
 	}*/
 
 	
