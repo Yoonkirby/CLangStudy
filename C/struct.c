05960e53a8766f4e0ea2045aeffa8c7114ae0ed7
Yoonkirby <yck93@naver.com>
2022-02-16 22:57:37 +0900

    구조체

--- C/struct.c
+++ C/struct.c
@@ -10,6 +10,16 @@ struct Gameinfo
    struct Gameinfo * friendGame; //연관업체게임
 };
 
+typedef struct Gameinformation //Gameinformation이 없어도 사용 가능
+{
+   char * name;
+   int year;
+   int price;
+   char * company;
+
+   struct Gameinfo * friendGame; //연관업체게임
+} GAME_INFO;
+
 int main()
 {
    //[게임 출시]
@@ -94,6 +104,23 @@ int main()
    printf("가격      : %d\n", gameinfo1.friendGame->price);
    printf("제작사    : %s\n", gameinfo1.friendGame->company);
 
+   //typedef
+   //자료형에 별명 지정
+   int i = 1;
+   typedef int 정수;
+   typedef float 실수;
+   정수 정수변수 = 3; // int i = 3;
+   실수 실수변수 = 3.23f; // float f= 3.23f;
+   printf("\n\n정수변수 : %d, 실수변수 : %.2f\n\n", 정수변수, 실수변수);
+
+   typedef struct Gameinfo 게임정보; // typedef (struct Gameinfo)를 '게임정보'로 바꿔줘 라는 뜻
+   게임정보 game1;
+   game1.name = "한글 게임";
+   game1.year = 2020;
+
+   GAME_INFO game2;
+   game2.name = "한글 게임2";
+   game2.year = 2019
 
    return 0;
 }
