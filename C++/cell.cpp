b2171c09696b0ab1ce2de6d5567749263cfe2198
Seongjune Yoon <yck93@naver.com>
2022-03-01 13:27:02 +0900

    C++ 시작

--- C++/cell.cpp
+++ C++/cell.cpp
@@ -1,10 +1,44 @@
 #include <iostream>
 
-using namespace std;
-
-int main()
+int main(void)
 {
-    cout << "안녕, World!" << endl;
+    //p.22 문제 1-1
+    //문제1
+    /*int num1,num2,num3,num4,num5,sum;
+    std::cout<<"Enter the first number : ";
+    std::cin>>num1;
+    std::cout<<"Enter the second number : ";
+    std::cin>>num2;
+    std::cout<<"Enter the third number : ";
+    std::cin>>num3;
+    std::cout<<"Enter the 4th number : ";
+    std::cin>>num4;
+    std::cout<<"Enter the 5th number : ";
+    std::cin>>num5;
+    sum = num1+num2+num3+num4+num5;
+    std::cout<<"SUM: "<<sum<<std::endl;*/
 
+    //문제2
+    /*char name[100];
+    char phone[100];
+
+    std::cout<<"Your name : ";
+    std::cin>>name;
+    std::cout<<"Your phone number : ";
+    std::cin>>phone;
+
+    std::cout<<"Your name is "<<name<<",Your phone number is "<<phone<<std::endl;*/
+
+    //문제3
+    /* int i,j;
+    std::cout<<"Enter : ";
+    std::cin>>i;
+    for(j = 1; j <= 9; j++)
+    {
+        std::cout<<i<<" * "<<j<<" = "<<i*j<<std::endl;
+    } */
+
+    
+    
     return 0;
 }
