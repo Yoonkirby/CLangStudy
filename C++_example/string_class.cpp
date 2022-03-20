87561b0587f8f78afc6499db6d5648f48347926f
Seongjune Yoon <yck93@naver.com>
2022-03-20 12:12:35 +0900

    이젠 더이상 원인을 모르겠다...

--- C++_example/string_class.cpp
+++ C++_example/string_class.cpp
@@ -8,68 +8,74 @@ private:
     int len;
     char * str;
 public:
-    String()
-    {
-        len = 0;
-        str = NULL;
-    }
-    String(const char * ref) : len(strlen(ref) + 1)
-    {
-        str = new char(len);
-        strcpy(str, ref);
-    }
-    String(const String& ref)
-    {
-        len = ref.len;
-        str = new char[len];
-        strcpy(str, ref.str);
-    }
-    ~String()
-    {
-        if(str != NULL)
-            delete []str;
-    }
-    String operator+(const String& ref)
-    {
-        char * tmpstr = new char[len + ref.len - 1];
-        strcpy(tmpstr, str);
-        strcat(tmpstr, ref.str);
-        String tmp(tmpstr);
-        delete []tmpstr;
-        return tmp;
-    }
-    String& operator=(const String& ref)
-    {
-        if(str != NULL)
-            delete []str;
-        len = ref.len;
-        str = new char[len];
-        strcpy(str, ref.str);
-        return *this;
-    }
-    String& operator+=(const String& ref)
-    {
-        /*len += (ref.len - 1);
-        char * tmpstr = new char[len];
-        strcpy(tmpstr, str);
-        strcat(tmpstr, ref.str);
-        if(str != NULL)
-            delete []str;
-        str = tmpstr;*/
-        *this = *this + ref;
-        return *this;
-    }
-    bool operator==(const String& ref)
-    {
-        if(str == ref.str)
-            return true;
-        else
-            return false;
-    }
-    friend ostream& operator<<(ostream& op, String& ref);
+    String();
+	String(const char * s);
+	String(const String& s);
+	~String();
+	String& operator= (const String& s);
+	String& operator+= (const String& s);
+	bool operator== (const String& s);
+    String operator+ (const String& s);
+
+    friend ostream& operator<<(ostream& op, const String& ref);
     friend istream& operator>>(istream& ip, String& ref);
 };
-ostream& operator<<(ostream& os, String& ref)
+String::String()
+{
+    len = 0;
+    str = NULL;
+}
+String::String(const char * ref) : len(strlen(ref)+1)
+{
+    str = new char(len);
+    strcpy(str, ref);
+}
+String::String(const String& ref)
+{
+    len = ref.len;
+    str = new char[len];
+    strcpy(str, ref.str);
+}
+String::~String()
+{
+    if(str!=NULL)
+        delete []str;
+}
+String& String::operator=(const String& ref)
+{
+    if(str!=NULL)
+        delete []str;
+    len = ref.len;
+    str = new char[len];
+    strcpy(str, ref.str);
+    return *this;
+}
+String& String::operator+=(const String& ref)
+{
+    len+=(ref.len-1);
+    char * tmpstr = new char[len];
+    strcpy(tmpstr, str);
+    strcat(tmpstr, ref.str);
+    if(str!=NULL)
+        delete []str;
+    str = tmpstr;
+    //*this = *this + ref;
+    return *this;
+}
+bool String::operator==(const String& ref)
+{
+    return strcmp(str, ref.str) ? false : true;
+}
+String String::operator+(const String& ref)
+{
+    char * tmpstr = new char[len+ref.len-1];
+    strcpy(tmpstr, str);
+    strcat(tmpstr, ref.str);
+    String tmp(tmpstr);
+    delete []tmpstr;
+    return tmp;
+}
+ostream& operator<<(ostream& os, const String& ref)
 {
     os<<ref.str;
     return os;
