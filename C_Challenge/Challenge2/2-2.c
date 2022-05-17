b1103b8de0b831464f5cad514ca0b36247ab766f
Seongjune Yoon <yck93@naver.com>
2022-05-17 22:11:43 +0900

    2-2품

--- C_Challenge/Challenge2/2-2.c
+++ C_Challenge/Challenge2/2-2.c
@@ -2,6 +2,28 @@
 
 int main(void)
 {
+    int bi[32];
+    int enter, i;
+
+    for(i = 0; i > 32; i++)
+        bi[i] = 0;
+    
+    printf("Enter Decimal Number : ");
+    scanf_s("%d",&enter);
+
+    i = 0;
+    while(enter != 0)
+    {
+        bi[i] = enter % 2;
+        enter = enter / 2;
+        i++;
+    }
+    while(i > 0)
+    {
+        i--;
+        printf("%d",bi[i]);
+    }
+    printf("\n");
 
     return 0;
 }
