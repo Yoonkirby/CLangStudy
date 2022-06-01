8d3f90170a1fd58b45ea7208999e56de7a313aa2
Seongjune Yoon <yck93@naver.com>
2022-06-01 11:20:35 +0900

    3-4

--- C_Challenge/Challenge3/3-4.c
+++ C_Challenge/Challenge3/3-4.c
@@ -1,8 +1,13 @@
-
+// 두개의 주사위를 던졌을 때의 결과를 출력
 #include <stdio.h>
+#include <stdlib.h> // rand 함수
+#include <time.h>   // time 함수
 
 int main(void)
 {
-    
+    srand(time(NULL));
+    printf("Dice 1 : %d\n", rand()%6+1);
+    printf("Dice 2 : %d\n", rand()%6+1);
+
     return 0;
 }
