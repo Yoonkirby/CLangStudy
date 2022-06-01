142b41c3b10a1b6591bda236f8d8a234900ccb53
Seongjune Yoon <yck93@naver.com>
2022-06-01 10:39:04 +0900

    3-2

--- C_Challenge/Challenge3/3-2.c
+++ C_Challenge/Challenge3/3-2.c
@@ -8,6 +8,53 @@
 
 int main(void)
 {
+    int enter, i, j, x = 0, y = 0, cnt = 1, pm = 1;
+    printf("Enter : ");
+    scanf_s("%d", &enter);
+    int arr[enter][enter];
+    
+    for(y = 0; y < enter; y++)
+    {
+        arr[x][y] = cnt++;
+    }
+    y--;
+
+    for(i = enter-1; i >= 1; i--)
+    {
+        if(pm == 1)
+        {
+            for(j = 0; j < i; j++)
+            {
+                arr[++x][y] = cnt++;
+            }
+            for(j = 0; j < i; j++)
+            {
+                arr[x][--y] = cnt++;
+            }
+            pm *= -1;
+        }
+        else if(pm == -1)
+        {
+            for(j = 0; j < i; j++)
+            {
+                arr[--x][y] = cnt++;
+            }
+            for(j = 0; j < i; j++)
+            {
+                arr[x][++y] = cnt++;
+            }
+            pm *= -1;
+        }
+    }
+    
+    for(i = 0; i < enter; i++)
+    {
+        for(j = 0; j < enter; j++)
+        {
+            printf("\t%d",arr[i][j]);
+        }
+        printf("\n\n");
+    }
 
     return 0;
 }
