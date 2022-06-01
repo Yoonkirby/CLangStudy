9f60d767eda565bf4600b87b5b8fbd8dd55e1642
Seongjune Yoon <yck93@naver.com>
2022-06-01 11:13:42 +0900

    3-3

--- C_Challenge/Challenge3/3-3.c
+++ C_Challenge/Challenge3/3-3.c
@@ -1,8 +1,14 @@
-
+// 0 이상 99 이하의 난수를 총 5개 생성
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(void)
 {
-
+    srand(time(NULL));
+    for(int i = 0; i < 5; i++)
+    {
+        printf("Random Number %d : %d\n", i+1, rand()%100);
+    }
     return 0;
 }
