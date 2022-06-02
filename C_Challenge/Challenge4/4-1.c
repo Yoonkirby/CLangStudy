6a4f0b1c27e4c2564659487b6ef0aa517c05a509
Seongjune Yoon <yck93@naver.com>
2022-06-02 10:44:45 +0900

    4-1

--- C_Challenge/Challenge4/4-1.c
+++ C_Challenge/Challenge4/4-1.c
@@ -1,8 +1,37 @@
-
+// 도서 관리용 프로그램
+// [제목, 저자명, 페이지수]에 대한 정보를 저장할 수 있는 구조체를 정의
+// 구조체 배열을 선언해서 도서에 대한 정보를 저장하는 구조로 작성
+// main함수에서 사용자로부터 3권의 도서에 대한 정보를 입력 받고, 입력이 끝나면 도서에 대한 내용을 출력
 #include <stdio.h>
+
+struct Book
+{
+    char title[50];
+    char author[25];
+    int page;
+};
 
 int main(void)
 {
-
+    struct Book book[3];
+    int i;
+    printf("Enter Book Information\n");
+    for(i = 0; i < 3; i++)
+    {
+        printf("Enter Author : ");
+        scanf_s("%s", book[i].author);
+        printf("Enter Title : ");
+        scanf_s("%s", book[i].title);
+        printf("Enter Pages : ");
+        scanf_s("%d", &book[i].page);
+    }
+    printf("\nPrint Book Information\n");
+    for(i = 0; i < 3; i++)
+    {
+        printf("Book %d\n", i+1);
+        printf("Author : %s\n", book[i].author);
+        printf("Title : %s\n", book[i].title);
+        printf("Pages : %d\n", book[i].page);
+    }
     return 0;
 }
--- C_Challenge/Challenge4/4-2.c
+++ C_Challenge/Challenge4/4-2.c
@@ -1,8 +1,58 @@
-
+// 4-1에서 구현한 프로그램에서 변경을 가함
+// 구조체 배열을 선언하는 것이 아니라, 구조체 포인터 배열을 선언하고 구조체 변수를 동적으로 할당하는 형태로 프로그램을 재 구현
+// 4-1에서 구현한 방법보다 4-2에서 구현한 방법이 지니는 장점이 무엇인지도 생각해보자
 #include <stdio.h>
+#include <stdlib.h> // malloc()
+#include <string.h>
+
+typedef struct Book
+{
+    char * title;
+    char * author;
+    int * page;
+}BOOK;
 
 int main(void)
 {
-
+    BOOK * book[3];
+    int i, temp[3];
+    char tmp[100];
+    printf("Enter Book Information\n");
+    for(i = 0; i < 3; i++)
+    {
+        printf("Enter Author : ");
+        scanf_s("%s", tmp);
+        temp[i] = strlen(tmp)+1;
+        book[i]->author = (char *)malloc(sizeof(char)*temp[i]);
+        printf("Author : %s\n", book[i]->author);
+        strcpy(book[i]->author, tmp);
+        
+        printf("Enter Title : ");
+        scanf_s("%s", tmp);
+        temp[i] = strlen(tmp)+1;
+        book[i]->title = (char *)malloc(sizeof(char)*temp[i]);
+        strcpy(book[i]->title, tmp);
+        
+        printf("Enter Pages : ");
+        scanf_s("%d", temp);
+        book[i]->page = (int *)malloc(sizeof(int));
+        book[i]->page = &temp[i];
+        
+        if(book[i]->author == NULL || book[i]->title == NULL || book[i]->page == NULL)
+        {
+            exit(1);
+        }
+    }
+    printf("\nPrint Book Information\n");
+    for(i = 0; i < 3; i++)
+    {
+        printf("Book %d\n", i+1);
+        printf("Author : %s\n", book[i]->author);
+        printf("Title : %s\n", book[i]->title);
+        printf("Pages : %d\n", book[i]->page);
+        free(book[i]->author);
+        free(book[i]->title);
+        free(book[i]->page);
+    }
     return 0;
 }
